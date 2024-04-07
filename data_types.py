e31a0d202552603f90ea89a9137e00035522e0e1
Vasu <vasumendapara93@gmail.com>
2024-04-07 13:13:12 +0530

    Data types in python

---
+++ data_types.py
@@ -0,0 +1,59 @@
+# string
+a = "Hello World"
+print(a)
+
+# integer
+b = 50
+print(b)
+
+# float
+c = 60.5
+print(c)
+
+# complex
+d = 3j
+print(d)
+
+# list
+e = ["geeks", "for", "geeks"] 
+print(e)
+
+# tuple
+f = ("geeks", "for", "geeks") 
+print(f)
+
+# range
+g = range(10) 
+print(g)
+
+# dictionary
+h = {"name": "Suraj", "age": 24} 
+print(h)
+
+# set
+i = {"geeks", "for", "geeks"} 
+print(i)
+
+# frozenset
+j = frozenset({"geeks", "for", "geeks"}) 
+print(j)
+
+# bool
+k = True
+print(k)
+
+# bytes
+l = b"Geeks"
+print(l)
+
+# byte array
+m = bytearray(4) 
+print(m)
+
+# memory view
+n = memoryview(bytes(6)) 
+print(n)
+
+# none type
+o = None
+print(o)
