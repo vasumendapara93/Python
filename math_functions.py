845e6bee1f6cbaa76c7cb9dc05129dcefe13d2a7
Vasu <vasumendapara93@gmail.com>
2024-05-11 13:12:01 +0530

    some useful math functions

---
+++ math_functions.py
@@ -0,0 +1,13 @@
+import math
+
+print(round(3.1))
+print(round(-3.1))
+print(abs(-3.1))
+print(min(5, 10, 25))
+print(max(5, 10, 25))
+print(pow(4, 3))
+
+print(math.sqrt(9))
+print(math.ceil(1.4))
+print(math.floor(1.4))
+print(math.pi)
